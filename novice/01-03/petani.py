d151949b56ed6d94c71de45c868e90c39395c8a5
irroel <irulnewmail@gmail.com>
2020-10-12 16:54:55 +0700

    petani.py rev

--- novice/01-03/petani.py
+++ novice/01-03/petani.py
@@ -1,24 +1,22 @@
 class Petani():
-    def __init__(self, name):
-        # self.lahan = lahan
+    def __init__(self, name, lahan):
+        self.lahan = lahan
         self.name = name
+    def getName(self):
+        return self.name
+    def getLahan(self):
+        return self.lahan
     # def membajak(self, lahan):
     #     self.lahan = lahan
         
 
 class Lahan(Petani):
     def __init__(self, name, lahan):
-        super.__init__(self, name, lahan)
-    def membajak(self, lahan):
-        self.lahan = lahan
-        return f'Pak {name}, membajak {lahan}.'
+        super().__init__(name, lahan)
+           
+    def membajak(self):
+        return Petani.getName(self) + " sedang membajak " + Petani.getLahan(self)
 
-p = Petani("Praxis")
+l = Lahan("Praxis", "sawah")
 
-print(p.name)
-
-
-l = Lahan("Prax", "sawah")
-print(l.name)
-print(l.lahan)
-l.membajak("sawah")
+l.membajak()
