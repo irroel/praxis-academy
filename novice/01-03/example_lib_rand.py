7fd93835e47c47d9d5f60529fa4fb568f29d0f5f
irroel <irulnewmail@gmail.com>
2020-10-13 13:57:19 +0700

    add file

---
+++ novice/01-03/example_lib_rand.py
@@ -0,0 +1,8 @@
+import random
+
+l = []
+r = random.randint(5,10)
+print(f'Bilangan random yang terpilih : {r}')
+for x in range (r):
+    l.append(x)
+print(l)
--- novice/01-03/petani.py
+++ novice/01-03/petani.py
@@ -1,22 +1,17 @@
-class Petani():
-    def __init__(self, name, lahan):
-        self.lahan = lahan
+class Petani:
+    all_petani = []                     # mengakses semua instance
+    def __init__(self, name, lahan):    # inisiasi
         self.name = name
-    def getName(self):
-        return self.name
-    def getLahan(self):
-        return self.lahan
-    # def membajak(self, lahan):
-    #     self.lahan = lahan
-        
+        self.lahan = lahan
+        Petani.all_petani.append(self)  # menambah setiap instance ke all_petani[]
 
-class Lahan(Petani):
-    def __init__(self, name, lahan):
-        super().__init__(name, lahan)
-           
-    def membajak(self):
-        return f'Pak {Petani.getName(self)}  sedang membajak  {Petani.getLahan(self)}'
+class Lahan(Petani):                    # Lahan inherit from Petani
+    def membajak(self, luas):
+        return f'Pak {self.name}, sedang membajak {self.lahan} seluas {luas} hektar'
 
-l = Lahan("Praxis", "sawah")
+class Bibit(Petani):                    # Bibit inherit from Petani
+    def menanam(self,bibit):
+        return f'{self.lahan.title()} pak {self.name} akan ditanami {bibit}.'
 
-l.membajak()
+
+print(Petani.all_petani)
